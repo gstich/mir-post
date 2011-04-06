9f8ea899a0d4a540d36a1af22c0cb758136964d2
britton.olson <britton.olson@aeafef00-c8ab-75f3-227c-5fe2c2ff918b>
2011-04-06 01:28:50 +0000

    Added matlab scripts...

--- trunk/src/tec2bin.f90
+++ trunk/src/tec2bin.f90
@@ -4,14 +4,40 @@ PROGRAM tec2bin
 
   INTEGER, PARAMETER :: flen = 100
   INTEGER, PARAMETER :: funit = 21
-  CHARACTER(LEN=flen) :: ftec,comments
-  INTEGER :: nx,ny
-  
+  CHARACTER(LEN=flen) :: ftec,fbin,comments
+  INTEGER :: nx,ny,nvar
+  INTEGER :: i,j,v
+  DOUBLE PRECISION, DIMENSION(:,:,:), ALLOCATABLE :: data
 
-  ftec = 
+  ftec = '/p/lscratchd/olson45/nozzle/nozzlecoarse3d/vis0020/planes/mean.tec'
+  fbin = '/p/lscratchd/olson45/nozzle/nozzlecoarse3d/vis0020/planes/mean.bin'
+  nx = 512
+  ny = 128
+  nvar = 8
+
+  ALLOCATE(data(nx,ny,nvar))
 
   OPEN(UNIT=funit,FILE=ftec,FORM='FORMATTED',STATUS='UNKNOWN')
   READ(funit,*) comments
-  READ(funit,*) comments,comments,nx,comments,comments,ny
-
+  READ(funit,*) comments
+  
+  DO j=1,ny
+     DO i=1,nx
+        READ(funit,*) data(i,j,1),data(i,j,2)
+        DO v=3,nvar
+           READ(funit,*) data(i,j,v)
+        END DO
+     END DO
+  END DO
   CLOSE(funit)
+
+
+  ! Dump the bin here
+  OPEN(UNIT=funit,FILE=fbin,FORM='FORMATTED',STATUS='UNKNOWN')
+  WRITE(funit,*) data
+  CLOSE(funit)
+
+  DEALLOCATE(data)
+  
+
+END PROGRAM tec2bin
