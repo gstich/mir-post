ae8427731432a43ad4b8ee7b2c18d7071fe657fd
britton.olson <britton.olson@aeafef00-c8ab-75f3-227c-5fe2c2ff918b>
2011-05-10 00:19:10 +0000

    added some matlab scripts for new data format

---
+++ trunk/src/matlab_scripts/load_planes_grid.m
@@ -0,0 +1,25 @@
+function data = load_planes_grid(path)
+
+% This script will open up the 2d span averaged data and operate on themes
+
+% Get the number of variables from the .mir file
+mir_path = [path,'/mean.mir'];
+[h,d] = hdrload(mir_path);
+len = sscanf( h(6,12:end),'%i');
+nx = len(1);
+ny = len(2);
+
+
+% Locate the plane data and load it in raw form
+data_path = [path, '/grid2d/p000000'];
+disp(data_path);
+funit = fopen(data_path);
+
+   
+% Put in (nx,ny) array
+data = zeros(nx,ny,2);   
+for i=1:2
+    data(:,:,i) = fread(funit,[nx ny],'single');
+end
+    
+fclose(funit);
---
+++ trunk/src/matlab_scripts/load_planes_mir.m
@@ -0,0 +1,27 @@
+function data = load_planes_mir(path,field,t)
+
+% This script will open up the 2d span averaged data and operate on themes
+
+% Get the number of variables from the .mir file
+mir_path = [path,'/mean.mir'];
+[h,d] = hdrload(mir_path);
+nvar = sscanf( h(10,12:end),'%i');
+len = sscanf( h(6,12:end),'%i');
+nx = len(1);
+ny = len(2);
+
+
+% Locate the plane data and load it in raw form
+data_path = [path, '/',field,num2str(t,'%04i'),'/p000000'];
+disp(data_path);
+funit = fopen(data_path);
+
+   
+% Put in (nx,ny) array
+data = zeros(nx,ny,nvar);   
+for i=1:nvar
+    data(:,:,i) = fread(funit,[nx ny],'single');
+end
+    
+   
+fclose(funit);
