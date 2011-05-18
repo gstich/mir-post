e42100d389e806d07ec2f1d7245aca450184f4a6
britton.olson <britton.olson@aeafef00-c8ab-75f3-227c-5fe2c2ff918b>
2011-05-18 21:33:24 +0000

    Updating to sync ptot rms plots

--- trunk/src/matlab_scripts/spectral.m
+++ trunk/src/matlab_scripts/spectral.m
@@ -1,43 +1,32 @@
-%function a = spectral(file)
-
-file = '../ptot_time.med2';
-data = load(file);
-
-time = data(:,1);
-p = data(:,5);
-u = data(:,2);
-v = data(:,3);
-w = data(:,4);
-rho = data(:,7);
-
-ptot = p + rho/2.*(u.*u + v.*v + w.*w);
+function  spectral
 
 
+npts = 5;
+Bfile = '../ptot_time.med';
 
-Ht = 1.78;
-Up = 32940*1.93;
-L=size(data,1);
-Fs = 200e3;
-hw = hanning(L,'periodic');
+b = 0;
+for i=1:npts
+    file = [Bfile, int2str(i) ];
+    data = load(file);
+    time = data(:,1);
+    p = data(:,5);
+    u = data(:,2);
+    v = data(:,3);
+    w = data(:,4);
+    rho = data(:,7);
+    ptot = p + rho/2.*(u.*u + v.*v + w.*w);
 
-y = ptot - mean(ptot);
-y = y/10;   % Convert to Pascals
-y = y.*y;
-y = y.*hw;
+    a = get_spectra(ptot);
+    b = b + a;
+end
 
-figure(1);
-plot(time,y);
+b = b/npts;
 
-NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-figure(2)
-Y = (fft(y,NFFT))/L;
-f = Fs/2*linspace(0,1,NFFT/2+1);
 
-% Plot single-sided amplitude spectrum.
-%semilogx(f,2*abs(f'.*Y(1:NFFT/2+1)));
-loglog(f*Ht/Up,2*abs(Y(1:NFFT/2+1))./f');hold all; 
-%xlim([10^-2.5 1]);
-%ylim([10^2 10^6]);
+
+loglog(b(1,:),b(2,:));hold all; 
+
+
 
 xlabel('f H_t/U_p')
 ylabel('Pa^2/Hz')
@@ -50,4 +39,37 @@ y = a*x.^m;
 hold on;
 plot(x,y,'k--');
 
-%end
+end
+
+
+
+
+
+function a = get_spectra(ptot)
+
+Ht = 1.78;
+Up = 32940*1.93;
+L=size(ptot,1);
+Fs = 200e3;
+hw = hanning(L,'periodic');
+
+y = ptot - mean(ptot);
+y = y/10;   % Convert to Pascals
+y = y.*y;
+y = y.*hw;
+
+
+NFFT = 2^nextpow2(L); % Next power of 2 from length of y
+figure(2)
+Y = (fft(y,NFFT))/L;
+f = Fs/2*linspace(0,1,NFFT/2+1);
+
+%% Divide by frequency
+
+a(1,:) = f*Ht/Up;
+a(2,:) = 2*abs(Y(1:NFFT/2+1))./f';
+
+
+
+
+end
