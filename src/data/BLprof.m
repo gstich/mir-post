a45d09f15bc549409c08ac1a0a89e4153679a8df
britton.olson <britton.olson@aeafef00-c8ab-75f3-227c-5fe2c2ff918b>
2011-05-19 05:39:34 +0000

    Cleaned up the figures

--- src/data/BLprof.m
+++ src/data/BLprof.m
@@ -3,20 +3,22 @@ clc;
 close all;
 
 %% Plot the BL data: Mean and Reynolds stress
-pdfE = false;
+pdfE = true;
 res(1).name = 'coarse';
 res(2).name = 'medium';
 res(3).name = 'fine';
-ref = 2;
+ref(1) = 1;
+ref(2) = 2;
+ref(3) = 2;
 
 % Figure name
 figs(1).name = 'mean';
 figs(2).name = 'rms';
 
 % Plot symbols
-res(1).sym = '--';
-res(2).sym = '-.';
-res(3).sym = '-';
+res(1).sym = 'k--';
+res(2).sym = 'k-.';
+res(3).sym = 'k-';
 
 % Figure option
 LW = 2;         % LineWidth
@@ -38,13 +40,6 @@ for i=1:size(res,2)
 end
 
 
-% Get reference utau,del,delBL,tau_w
-utau = res(ref).data(1,key.utau);
-del = res(ref).data(1,key.del);
-delBL = res(ref).data(1,key.delBL);
-tauw = res(ref).data(1,key.tauw);
-
-
 x1 = logspace(-1,1.2,20);
 x2 = logspace(.8,3,20);
 %k = .375;
@@ -59,6 +54,13 @@ semilogx(x2,1/k*log(x2)+C,'k--');hold on;
 
 
 for i=1:size(res,2)
+    
+    % Get reference utau,del,delBL,tau_w
+    utau = res(ref(i)).data(1,key.utau);
+    del = res(ref(i)).data(1,key.del);
+    delBL = res(ref(i)).data(1,key.delBL);
+    tauw = res(ref(i)).data(1,key.tauw);
+    
     y = res(i).data(:,key.y) / del; y = y - y(1);
     u = res(i).data(:,key.u) / utau;
     utau_i = res(i).data(1,key.utau);
@@ -75,6 +77,7 @@ set(h1,'Interpreter','latex','FontSize',FSn);
 h2 = ylabel('$u^+$');
 set(h2,'Interpreter','latex','FontSize',FSn);
 set(gca,'FontSize',FSa);
+%legend('Coarse','Medium','Fine');
 
 
 figure(2);clf;
@@ -90,13 +93,8 @@ end
 figure(2);
 xlim([0 1.2])
 ylim([0 3.5])
-box on;
-h1 = xlabel(['$y/ \delta$']);
-set(h1,'Interpreter','latex','FontSize',FSn);
-h2 = ylabel('$ \sqrt{u^\prime_i u^\prime_i} / u_\tau $');
-set(h2,'Interpreter','latex','FontSize',FSn);
-set(gca,'FontSize',FSa);
 
+%legend('Coarse','Medium','Fine');
 
 % Save the figures and convert them to .pdf
 if (pdfE)
--- src/data/span_corr.m
+++ src/data/span_corr.m
@@ -9,6 +9,9 @@ res(1).name = 'coarse';
 res(2).name = 'medium';
 ref = 2;
 
+% Figure name
+figs(1).name = '2dspancorr';
+figs(2).name = '1dspancorr';
 
 % Plot symbols
 res(1).sym = '--';
@@ -20,36 +23,65 @@ FSn = 18;       % FontSize labels
 FSa = 12;       % FontSize axis
 cmax = .2;      % Contour Level Max
 Nc = 64;        % Number of contour level
+Ht = 1.78;
 
 %% Coarse
 figure(1);
 load('span_corr/cor.mat');
-contourf(XX,YY+2,tay,[0:.01:.2],'edgecolor','none')
+XX = XX/Ht;
+YY = YY/Ht;
+contourf(XX,YY+1,tay,[0:.01:.2],'edgecolor','none')
 
 
 %% Correlation plots
 figure(2);hold on;
 x = [1:1:size(Cor,1)];
 
-plot(  x/x(end) *.5, Cor(:,1) ,'--');
-plot(  x/x(end) *.5, Cor(:,106) ,'--');
-plot(  x/x(end) *.5, Cor(:,399), '--');
+plot(  x/x(end) *.5, Cor(:,1) ,'--','LineWidth',LW);
+plot(  x/x(end) *.5, Cor(:,106) ,'--','LineWidth',LW);
+plot(  x/x(end) *.5, Cor(:,399), '--','LineWidth',LW);
 
 %% Medium
 figure(1);hold on;
 load('span_corr/med.mat');
-contourf(XX,YY-2,tay,[0:cmax/Nc:cmax],'edgecolor','none')
+XX = XX/Ht;
+YY = YY/Ht;
+contourf(XX,YY-1,tay,[0:cmax/Nc:cmax],'edgecolor','none')
 colorbar;
 axis equal
+box on;
+h1 = xlabel(['$x/ H_t$']);
+set(h1,'Interpreter','latex','FontSize',FSn);
+h2 = ylabel('$ y/H_t $');
+set(h2,'Interpreter','latex','FontSize',FSn);
+set(gca,'FontSize',FSa);
+
+
 
 %% Correlation plots
 figure(2);hold on;
 x = [1:1:size(Cor,1)];
-plot(  x/x(end) *.5, Cor(:,1) );
-plot(  x/x(end) *.5, Cor(:,106) );
-plot(  x/x(end) *.5, Cor(:,237) );
+plot(  x/x(end) *.5, Cor(:,1) ,'LineWidth',LW);
+plot(  x/x(end) *.5, Cor(:,106) ,'LineWidth',LW);
+plot(  x/x(end) *.5, Cor(:,237) ,'LineWidth',LW);
+box on;
+h1 = xlabel(['$z/ H_t$']);
+set(h1,'Interpreter','latex','FontSize',FSn);
+h2 = ylabel('$ R_{11}(u) $');
+set(h2,'Interpreter','latex','FontSize',FSn);
+set(gca,'FontSize',FSa);
 
 
+% Save the figures and convert them to .pdf
+if (pdfE)
+    for i=1 : size (figs , 2)
+        fname = [ '../figs/',figs(i).name , '.eps' ];
+        figure(i);
+        print('-depsc2',fname)
+        eps2pdf(fname)
+        delete(fname)
+    end
+end
 
 
 
