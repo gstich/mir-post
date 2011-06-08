b761194e1afec522292bf0beae2080401b4dd00a
britton.olson <britton.olson@aeafef00-c8ab-75f3-227c-5fe2c2ff918b>
2011-06-08 21:08:54 +0000

    added the exp. montage maker

---
+++ src/data/experiment/NOZZLE_DATA/SCHLIEREN/exp_montage.py
@@ -0,0 +1,89 @@
+## Script to take the experimental images, rotate them, make them B&W
+## and put them in an arbitrary order
+import os
+
+Nx = 2048
+Ny = 1536
+base='NWM059';
+fmt='.JPG'
+Iname = []
+Iname.append('15')
+Iname.append('16')
+Iname.append('18')
+Iname.append('19')
+Iname.append('20')
+Iname.append('21')
+
+for i in range(len(Iname)):
+    Iname[i] = base + Iname[i] + fmt;
+    
+
+## Make tmp workspace
+cmd = 'mkdir tmp'
+os.system(cmd)
+
+## Copy all the images
+for i in range(len(Iname)):
+    cmd = 'cp WALL/' + Iname[i] + ' tmp/' + Iname[i]
+    os.system(cmd)
+
+## Rotate all the images
+#for i in range(len(Iname)):
+#    cmd = 'cd tmp; mogrify -rotate 180 ' + Iname[i]
+#    os.system(cmd)
+
+
+## Crop to remove black
+Nyy = int(Ny/2.8);
+yoff = int(Ny/7.25);
+crop_args = " -crop " + str(Nx)+"x" + str(Nyy)+ "+0+" + str(yoff) + " ";
+for i in range(len(Iname)):
+    cmd = 'cd tmp; convert ' + Iname[i] + crop_args + Iname[i]
+    os.system(cmd)
+
+## Flip some images to get large lambda on top
+cmd = "cd tmp; mogrify -flip " + Iname[1]
+os.system(cmd);
+
+## Make a montage of the images
+ii = [0]*len(Iname)
+ii[0] = 2;
+ii[1] = 5;
+ii[2] = 4;
+ii[3] = 3;
+ii[4] = 0;
+ii[5] = 1;
+
+mN = 6;                    # Number of images in montage
+bdr = 5;                   # Border size
+mNx = Nx                   # Nx
+mNy = Nyy*mN + bdr*(mN+1)  # Ny
+ofile = 'test.jpg'
+
+files = ' ';
+for i in range(len(Iname)):
+    j = ii[i]
+    files = files + ' ' + Iname[j] + " "
+
+cmd = "cd tmp; montage -border " + str(bdr) + " -bordercolor white " +  " -geometry "+ str(mNx)+"x"+ " -tile 1x"+str(mN)+files+ofile
+os.system(cmd)
+
+
+## Make a grayscale image
+cmd = "cd tmp; mogrify -colorspace Gray " + ofile
+os.system(cmd)
+
+## Vary the brightness
+cmd = "cd tmp; mogrify -modulate 150 " + ofile
+os.system(cmd)
+
+## Clean up stuff
+cmd = "cp tmp/"+ofile+" montage.jpg"
+os.system(cmd)
+
+cmd = 'rm -rf tmp'
+os.system(cmd)
+
+print "Done with montage:"
+print str(mN) + " images written"
+print str(mNx)+"x"+str(Nyy)+" (*"+str(mN)+")"
