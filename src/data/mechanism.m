faa79110eb63328af9a5fce3f28cd80b7b0cf935
britton.olson <britton.olson@aeafef00-c8ab-75f3-227c-5fe2c2ff918b>
2011-06-17 00:22:21 +0000

    Edits and phase plots

--- src/data/mechanism.m
+++ src/data/mechanism.m
@@ -5,7 +5,7 @@ clc;
 % Figure name
 figs(1).name = 'mech_phase';
 
-pdfE = false;
+pdfE = true;
 
 %res = 'coarse';
 res = 'medium';
@@ -50,7 +50,7 @@ figure(1);hold on;
 toff = 550;
 
 t1 = toff;
-tf = 1213;
+tf = 1113;
 time = time*Up/Ht/1e3;
 time2 = time(t1:tf)-time(t1);
 shk = XSS(t1:tf,3);
@@ -59,13 +59,14 @@ for i=1:50
 shk = gfilter(shk);
 end
 
+shk = shk / top_bot_bound(shk);
 plot(time2,shk,'k','LineWidth',LW);hold on;
 
 
 % Load the exit plane data
 
 dt = time2(2)-time2(1);
-exit_data = load('mechanism/exit_plane.med.full');
+%exit_data = load('mechanism/exit_plane.med.full');
 exit_data = load('mechanism/exit_plane.med.full.wide');
 time3 = exit_data(:,1)-exit_data(1,1) - toff;
 time3 = time3*dt;
@@ -73,20 +74,23 @@ time3 = time3*dt;
 pexit = exit_data(:,5);
 pave = mean(pexit);
 pexit = (pexit-pave)/pave;
-for i=1:50
+for i=1:500
 pexit = gfilter(pexit);
 end
 
 uexit = exit_data(:,2);
 uave = mean(uexit);
 uexit = (uexit-uave)/uave;
-for i=1:50
+for i=1:500
 uexit = gfilter(uexit);
 end
 
+
+pexit = pexit / top_bot_bound(pexit(t1:end-200));
+uexit = uexit / top_bot_bound(uexit(t1:end-200));
 figure(1);
-plot(time3,pexit*10,'b','LineWidth',LW);hold on;
-plot(time3,uexit*5,'r','LineWidth',LW);hold on;
+plot(time3,pexit,'b','LineWidth',LW);hold on;
+plot(time3,uexit,'r','LineWidth',LW);hold on;
 
 
 %% Load the separation region 
@@ -98,6 +102,7 @@ delL = sep_data(:,3);
 core = delU-delL;
 core = (core-mean(core)) * 2 ;%/(1.78*1.6); 
 core = gfilter(core);
+core = core/top_bot_bound(core);
 
 figure(1);hold on;
 plot(time4,core,'g','LineWidth',LW);
@@ -107,16 +112,21 @@ figure(1);hold on;
 plot(xline,0*xline,'k-.','LineWidth',1)
 
 xlim([0 66]);
-ylim([-1. 1.]);
+ylim([-2.0 2.0]);
 box on;
 h1 = xlabel(['$tU_p/H_t$']);
 set(h1,'Interpreter','latex','FontSize',FSn);
-h2 = ylabel('$X_s / H_t$');
-set(h2,'Interpreter','latex','FontSize',FSn);
+%h2 = ylabel('$X_s / H_t$');
+%set(h2,'Interpreter','latex','FontSize',FSn);
 set(gca,'FontSize',FSa);
-h3 = legend('Shock','$P_{exit}$','$U_{exit}$','$A_{eff}$');
+h3 = legend('$X_{shock}$','$P_{exit}$','$U_{exit}$','$A_{exit}$');set(gcf,'Position',[1000,395,560,480])
 set(h3,'Interpreter','latex','FontSize',FSn);
 legend boxoff;
+
+h = figure(1);
+set(h,'Position',[1000,395,560*1.5,420*1.5])
+set(h,'PaperSize',[8,7]);
+set(h,'PaperPositionMode','auto');
 
 
 
