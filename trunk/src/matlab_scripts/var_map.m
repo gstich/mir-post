9ed61d09b054b1bc4124d438c9bdf6153b236aae
britton.olson <britton.olson@aeafef00-c8ab-75f3-227c-5fe2c2ff918b>
2012-03-02 01:10:45 +0000

    Varmap update

--- trunk/src/matlab_scripts/var_map.m
+++ trunk/src/matlab_scripts/var_map.m
@@ -1,5 +1,8 @@
 % Some constants used to reference the data-set
 
 % Some parameters for ease of use
-X=20; Y=21; U=1; V=2; W=3; RHO=5; P=4; T=6;UU=7;VV=8;WW=9;
+%X=20; Y=21; 
+X=21; Y=22; 
+
+U=1; V=2; W=3; RHO=5; P=4; T=6;UU=7;VV=8;WW=9;
 MU=16;MUa=17;MUb=18;MUc=19;
